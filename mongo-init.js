a3f48e9ed110d33b92d1a915f414d9a79e9f688c
Andrew logan <53392022+dev-alt@users.noreply.github.com>
2025-01-11 13:39:54 +1300

    Update MongoDB initialization script with strong passwords for admin and application-specific users

--- mongo-init.js
+++ mongo-init.js
@@ -4,7 +4,7 @@
 // If you are always creating the root user on startup, you don't need this
 db.createUser({
     user: "admin",
-    pwd: "your_admin_password", // Set a strong password for the admin user
+    pwd: "lXC2L98i91dlythSizdYiB0QNQl36E+uPqFN5QhdHzY=", // Set a strong password for the admin user
     roles: [
         { role: "userAdminAnyDatabase", db: "admin" }
     ]
@@ -15,7 +15,7 @@ db = db.getSiblingDB('projectnexus');
 // Create an application-specific user with limited privileges
 db.createUser({
     user: "projectnexus_user",
-    pwd: "your_new_secure_password", // *** Use the newly generated password here ***
+    pwd: "lXC2L98i91dlythSizdYiB0QNQl36E+uPqFN5QhdHzY=", // *** Use the newly generated password here ***
     roles: [
         { role: "readWrite", db: "projectnexus" }
     ]
